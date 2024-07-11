a60e09bf0b6650cfb5cd866a5817a3a18d6229cc
Ivachesk <ivachesk0@gmail.com>
2024-07-11 15:40:40 -0300

    Opção de saida
    incluso no sistema a opção de saida para o usuário.

--- Cartorio.c
+++ Cartorio.c
@@ -129,6 +129,7 @@ int main()
 		printf("\t1- Resgitrar nomes \n");
 		printf("\t2- Consultar nomes \n");
     	printf("\t3- Deletar nomes \n\n"); 
+    	printf("\t4- Sair do sistema\n\n");
     	printf("Opção:");  //Fim do menu 
 	
     	scanf("%d", &opcao); //Armazenando a escolha do usuário
@@ -149,6 +150,11 @@ int main()
 			case 3:
 			Deletar();
 			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;	
 			
 			default:
 			printf("Essa opção não está disponivel!\n");
